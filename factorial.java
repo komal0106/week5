72b6e814a42d4398b94b69db6982d1ae6560391b
komal0106 <patalaykomalsai@gmail.com>
2026-08-13 15:03:04 +0530

    Uncomment main method in factorial.java

--- factorial.java
+++ factorial.java
@@ -10,10 +10,10 @@ public class factorial{
         return result;
     }
 
-   /* public static void main(String[] args){
+    public static void main(String[] args){
         int number = 5; // Example input
         long factorial = compute(number);
         System.out.println("Factorial of " + number + " is: " + factorial);
     }
-    */
+    
 }
